72c7c797a1903f650d5f8b9e1e536132a29fa41d
peterjames3 <kamauchief97@gmail.com>
2024-10-10 12:51:55 +0300

    updated version

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next'
+export const metadata:Metadata = {
+  title: "Customers",
+ 
+}
 export default function page() {
   return <p className="text-2xl">Customers</p>;
 }
--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <p className="text-2xl">Dashboard Page</p>;
-}
